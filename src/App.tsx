86ff431fac0101f85ad93dc59deacb46054d3cd8
scotty974 <fabienethevepro@gmail.com>
2023-12-07 17:36:01 +0100

    ajout du timer et redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,39 @@
-
-import './App.css'
-
+import "./App.css";
+import Timer from "./components/Timer";
 function App() {
- 
-
   return (
     <>
-      <div className="bg-[url('/img/gta6Cover2.jpg')]">
-        <div className='flex flex-col h-screen items-center justify-center'>
-          <div className='flex flex-col gap-4'>
-            <span className='text-center text-xl'>Available on :</span>
-            <div className='flex flex-row gap-6 '>
-              <div className='play w-16 h-10 bg-bluePlay-opacity-30 border-2 rounded-2xl border-bluePlay'>
-                <span className='text-xl text-white'>PS5</span>
-              </div>
-              <div className='xbox w-16 h-10 bg-greenXbox-opacity-30 border-2 rounded-2xl border-greenXbox'>
-                <span className='text-xl text-white'>XBOX</span>
-              </div>
+      <main className="bg-[url('/img/gta6Cover.png')] bg-center bg-cover h-screen py-8 ">
+        <section className="container m-auto brightness-none">
+          <div className="flex flex-col">
+            <h2 className="text-center text-white text-lg my-8">
+              Available on {" "}
+            </h2>
+            <div className="flex justify-around w-40 m-auto">
+              <h4 className="text-lg text-white font-bold bg-blue-600 bg-opacity-30 p-1 px-3 rounded-full border border-blue-800">
+                PS5
+              </h4>
+              <h4 className="text-lg text-white font-bold bg-green-600 bg-opacity-30 p-1 px-3 rounded-full border border-green-800">
+                XBOX
+              </h4>
             </div>
           </div>
-          <div className='flex flex-row'>
-            <span className='divider1 w-[366px] h-05 
-             bg-white' ></span>
-            <div className='flex flex-col'>
-              <span className='text-center text-8xl'>GTA VI</span>
-              <span className='text-center text-3xl text-white'>Released Date</span>
-            </div>
-            <span className='divider2 w-[366px]  bg-white'></span>
+          <div className="flex justify-center items-center my-24">
+            <span className="h-0.5 bg-white w-96"></span>
+            <h1 className="text-center text-4xl text-white" >
+              <strong className="text-9xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-purple-600 to-orange">
+                GTA VI
+              </strong>
+              <br /> released date
+            </h1>
+            <span className="h-0.5 bg-white w-96"></span>
           </div>
-        </div>
-      </div>
+          <Timer />
+         <div className="flex justify-center my-14"> <a href="https://www.youtube.com/watch?v=QdBZY2fkU-0" target="_blank" className="text-white font-bold text-xl border-2 p-3 hover:bg-white hover:text-black">Watch Trailer</a></div> 
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Timer.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+
+export default function Timer(){
+   
+   
+const calculateDate = () =>{
+    const now:any = new Date();
+    const targetDate:any = new Date('December 31, 2025 23:59:59 GMT+00:00')
+    const timeRemaining = targetDate - now;
+
+    if(timeRemaining <= 0 ){
+        return{
+            days : 0,
+            hours : 0,
+            minutes : 0,
+            seconds : 0
+        }
+    }
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+}
+    
+
+const [timeRemaining, setTimeRemaining] = useState(calculateDate)
+
+    useEffect(()=>{
+        const inteval = setInterval(()=>{
+            setTimeRemaining(calculateDate())
+        },1000)
+        return ()=> clearInterval(inteval)
+    },[])
+
+
+    return(
+        <>
+        <div className="flex justify-around">
+            <div className="flex flex-col items-center">
+                <p className="text-8xl font-bold text-orange">{timeRemaining.days}</p>
+                <h4 className="text-5xl text-white">Days</h4>
+            </div>
+            <div className="flex flex-col items-center">
+                <p className="text-8xl font-bold text-orange">{timeRemaining.hours}</p>
+                <h4 className="text-5xl text-white">hours</h4>
+            </div>
+            <div className="flex flex-col items-center">
+                <p className="text-8xl font-bold text-orange">{timeRemaining.minutes}</p>
+                <h4 className="text-5xl text-white">Minutes</h4>
+            </div>
+            <div className="flex flex-col items-center">
+                <p className="text-8xl font-bold text-orange">{timeRemaining.seconds}</p>
+                <h4 className="text-5xl text-white">secondes</h4>
+            </div>
+        </div>
+        </>
+    )
+
+}
